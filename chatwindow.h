6ef29e429393cca205406cc03e22619b9560bb83
Syed Nabeel <fgnabeel1@gmail.com>
2025-04-17 15:13:59 +0500

    Update chatwindow.h

--- chatwindow.h
+++ chatwindow.h
@@ -7,21 +7,28 @@
 #include <QPushButton>
 #include <QNetworkAccessManager>
 
-class ChatWindow : public QWidget {
+class ChatWindow : public QWidget
+{
     Q_OBJECT
 
 public:
-    ChatWindow(const QString &doctorType, QWidget *parent = nullptr);
+    ChatWindow(const QString &doctorType, const QString &doctorName, QWidget *parent = nullptr);
+    ~ChatWindow();
 
 private slots:
     void sendMessage();
 
 private:
     QString doctorType;
+    QString doctorName;
     QTextEdit *chatBox;
     QLineEdit *inputField;
     QPushButton *sendButton;
     QNetworkAccessManager *networkManager;
+
+    void setupUI();
+    void applyStyle();
+    void sendInitialGreeting();
 };
 
 #endif // CHATWINDOW_H
